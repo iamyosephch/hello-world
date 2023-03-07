7bbc1add7386faf90f285658cbd570fc00b4ad17
Yoseph Christian <124603259+iamyosephch@users.noreply.github.com>
2023-03-07 11:05:20 +0700

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cin >> x;
+    cin >> y;
+    //Display Hello World
+    cout << "x + y = " << x + y << endl;
+    return 0;
+}
