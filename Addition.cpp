c42e7abd12b35852ab8d0234f303381bba9219c9
Yoseph Christian <124603259+iamyosephch@users.noreply.github.com>
2023-03-07 11:06:01 +0700

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int x;
-    int y;
+    int x; // Input variable x
+    int y; // Input variable y
     cin >> x;
     cin >> y;
-    //Display Hello World
+    //Display result of addition
     cout << "x + y = " << x + y << endl;
     return 0;
 }
