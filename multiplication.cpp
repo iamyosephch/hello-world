ed168806c8771d38abebcac9e5ecf4728eec593c
Yoseph Christian <124603259+iamyosephch@users.noreply.github.com>
2023-03-15 11:46:42 +0700

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,18 @@
+//Programming for multiplying number
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+// Input variable
+  int x;
+  int y;
+  // Input variable x
+  cin >> x;
+  // Input variable y
+  cin >> y;
+
+  cout << "Hasil dari x * y = " << x * y;
+  
+}
